6713afa8c44d55463ae0ac8ad9cd87657cceb3e0
Yassine Sekkah <yassinesekkah11@gmail.com>
2026-05-09 10:52:33 +0100

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { Login } from './features/auth/login/login';
+import { Register } from './features/auth/register/register';
+import { PostList } from './features/posts/post-list/post-list';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+      path: '',
+      redirectTo: 'login',
+      pathMatch: 'full'
+    },
+
+    {
+        path: 'login',
+        component: Login
+    },
+
+    {
+        path: 'register',
+        component: Register
+    },
+
+    {
+        path: 'posts',
+        component: PostList
+    }
+];
---
+++ src/app/features/posts/post-list/post-list.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-post-list',
+  imports: [RouterLink],
+  templateUrl: './post-list.html',
+  styleUrl: './post-list.css',
+})
+export class PostList {
+  title = 'Angular Posts';
+
+  showMessage(){
+    alert('Hello Angular');
+  }
+
+  imageUrl = 'https://angular.dev/assets/images/press-kit/angular_icon_gradient.gif';
+
+  count = 0;
+
+  icrement(){
+    this.count++;
+  }
+
+  isLoggedIn = true;
+
+  posts = [
+    'Angular',
+    'Spring Boot',
+    'Jwt Security'
+  ];
+}
